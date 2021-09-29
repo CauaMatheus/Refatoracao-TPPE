6308b9c1374d8995359ddde536bd46f8b49aa570
Peedrooo <pedrovitora.jesus@gmail.com>
2021-09-29 20:05:55 -0300

    Alteração Layout

--- src/interfac/Menu.java
+++ src/interfac/Menu.java
@@ -14,11 +14,10 @@ public class Menu implements ActionListener {
 
 	// Criando paramentros paras as janelas
 
-	private static JFrame janela = new JFrame("Menu");
-	private static JLabel titulo = new JLabel("Menu Principal");
+	private static JFrame janela = new JFrame("Menu Principal");
+	private static JLabel titulo = new JLabel("Cinefun");
 	private static JButton filme = new JButton("Filme"); // aluno -> filme
 	private static JButton ingresso = new JButton("Ingresso"); // curso -> ingresso
-	private static JButton espectador = new JButton("Espectador"); // professor -> espectador
 
 	public static ControleDados dados = new ControleDados();
 
@@ -27,10 +26,9 @@ public class Menu implements ActionListener {
 		// Determinando tamanho dos ícones
 
 		titulo.setFont(new Font("Arial", Font.BOLD, 20));
-		titulo.setBounds(120, 10, 150, 30);
-		filme.setBounds(140, 50, 100, 30);
-		ingresso.setBounds(140, 100, 100, 30);
-		espectador.setBounds(140, 150, 100, 30);
+		titulo.setBounds(160, 10, 150, 50);
+		filme.setBounds(95, 70, 200, 40);
+		ingresso.setBounds(95, 125, 200, 40);
 
 		// Gerando janela com os parâmetros especificados
 
@@ -39,7 +37,6 @@ public class Menu implements ActionListener {
 		janela.add(titulo);
 		janela.add(filme);
 		janela.add(ingresso);
-		janela.add(espectador);
 
 		// Determinando tamanho padrão da janela
 
@@ -54,7 +51,6 @@ public class Menu implements ActionListener {
 		Menu menu = new Menu();
 
 		filme.addActionListener(menu);
-		espectador.addActionListener(menu);
 		ingresso.addActionListener(menu);
 	}
 
@@ -68,11 +64,8 @@ public class Menu implements ActionListener {
 		if (src == filme)
 			new TelaFilme().mostrarDados(dados);
 
-		// if(src == ingresso) new TelaIngresso().mostrarDados(dados,2) ;
+		if (src == ingresso)
+			new TelaIngresso().mostrarDados(dados);
 
-		if (src == espectador)
-			new TelaEspectador().mostrarDados(dados);
-
-		;
 	}
 }
--- src/interfac/TelaIngresso.java
+++ src/interfac/TelaIngresso.java
@@ -1,5 +1,81 @@
 package interfac;
 
-public class TelaIngresso {
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import control.ControleDados;
+
+public class TelaIngresso implements ActionListener, ListSelectionListener {
+	private JFrame janela;
+	private JLabel titulo;
+	private JButton cadastroIngresso;
+	private JButton refreshEspectador;
+	private JButton refreshFilme;
+	private static ControleDados dados;;
+	private JList<String> listaEspectadoresCadastrados;
+
+	public void mostrarDados(ControleDados d) {
+		dados = d;
+		listaEspectadoresCadastrados = new JList<String>();
+		janela = new JFrame("Espectadores Cadastrados");
+		titulo = new JLabel("Espectadores");
+		cadastroIngresso = new JButton("Novo");
+		refreshFilme = new JButton("Refresh");
+
+		titulo.setFont(new Font("Arial", Font.BOLD, 20));
+		titulo.setBounds(125, 10, 250, 30);
+
+		listaEspectadoresCadastrados.setBounds(20, 50, 350, 120);
+		listaEspectadoresCadastrados.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		listaEspectadoresCadastrados.setVisibleRowCount(10);
+
+		cadastroIngresso.setBounds(70, 177, 100, 30);
+		refreshFilme.setBounds(200, 177, 100, 30);
+
+		janela.setLayout(null);
+		janela.setResizable(false);
+		janela.setLocationRelativeTo(null);
+
+		janela.add(titulo);
+		janela.add(listaEspectadoresCadastrados);
+		janela.add(cadastroIngresso);
+		janela.add(refreshFilme);
+
+		janela.setSize(400, 250);
+		janela.setVisible(true);
+
+		cadastroIngresso.addActionListener(this);
+		refreshFilme.addActionListener(this);
+		listaEspectadoresCadastrados.addListSelectionListener(this);
+
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent acao) {
+		// TODO Auto-generated method stub
+		
+		Object gatilho = acao.getSource();
+		
+		if (gatilho == cadastroIngresso) {
+			new 
+		}
+
+
+	}
 
 }
