82ad3fcc7ab99e6c108185472917c708b10e454d
Peedrooo <pedrovitora.jesus@gmail.com>
2021-09-30 20:39:50 -0300

    Interface filme completa

--- src/interfac/TelaEdit.java
+++ src/interfac/TelaEdit.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import control.ControleDados;
@@ -15,196 +16,254 @@ public class TelaEdit implements ActionListener {
 	private JFrame janela;
 	private JLabel labelNome = new JLabel("Nome: ");
 	private JTextField valorNome;
-	private JLabel labelHoraAula = new JLabel("Valor Hora/Aula (R$): ");
-	private JTextField valorHoraAula;
-	private JLabel labelEnd = new JLabel("Endereco: ");
-	private JTextField valorEnd;
-	private JLabel labelCPF = new JLabel("CPF: ");
-	private JTextField valorCPF;
-	private JLabel labelID = new JLabel("Numero da Identidade: ");
-	private JTextField valorID;
-	private JLabel labelTelefone = new JLabel("Telefone");
-	private JTextField valorDDD;
-	private JTextField valorTelefone;
+	private JLabel labelSala = new JLabel("Sala: ");
+	private JTextField valorSala;
+	private JLabel labelHora = new JLabel("Hora: ");
+	private JTextField valorHora;
+	private JLabel labelDura = new JLabel("Duração: ");
+	private JTextField valorDura;
+	private JLabel labelGenero = new JLabel("Genero");
+	private JTextField valorGenero;
+	private JLabel Labelvalor = new JLabel("valor: ");
+	private JTextField valorValor;
+	private JLabel labelFaixa = new JLabel("Faixa Etaria: ");
+	private JTextField valorFaixa;
+	private JLabel labelDim = new JLabel("Dimensão: ");
+	private JTextField valorDim;
+	private JLabel labelAudio = new JLabel("Audio: ");
+	private JTextField valorAudio;
+	private JLabel labelEspCad = new JLabel("Espectadores: ");
+	private JTextField valorEspCad;
+
 	private JButton botaoExcluir = new JButton("Excluir");
 	private JButton botaoSalvar = new JButton("Salvar");
-	private String[] novoDado = new String[9];
+	private String[] novoDado = new String[12];
 	private static ControleDados dados;
 	private int posicao;
 	private int opcao;
-	private String dado;
+	private String s;
 
-	// Inserindo na janela
-	public void inserirEditar() {
-		dado = "5";
-		// Determinado layout
+	public void inserirEditar(int op, ControleDados d, TelaFilme p, int pos) {
 
-		valorNome = new JTextField(200);
-		valorEnd = new JTextField(200);
-		valorHoraAula = new JTextField(200);
-		valorCPF = new JTextField(200);
-		valorID = new JTextField(200);
-		valorDDD = new JTextField(3);
-		valorTelefone = new JTextField(10);
-		botaoSalvar.setBounds(245, 175, 115, 30);
+		opcao = op;
+		posicao = pos;
+		dados = d;
 
-		janela = new JFrame(dado);
+		if (op == 1)
+			s = "Cadastro Filme";
+		if (op == 2)
+			s = "Cadastro Ingresso";
+		if (op == 3)
+			s = "Edição Filme";
+		if (op == 4)
+			s = "Edição Ingresso";
+
+		janela = new JFrame(s);
+
+		// Preenche dados com dados do aluno clicado
+		if (op == 3) {
+			valorNome = new JTextField(dados.getFilmes()[pos].getNomeFilme(), 200);
+			valorSala = new JTextField(String.valueOf(dados.getFilmes()[pos].getNumSala()), 200);
+			valorHora = new JTextField(dados.getFilmes()[pos].getHoraFilme(), 200);
+			valorDura = new JTextField(String.valueOf(dados.getFilmes()[pos].getDuracao()), 200);
+			valorGenero = new JTextField(dados.getFilmes()[pos].getGenero(), 200);
+			valorValor = new JTextField(String.valueOf(dados.getFilmes()[pos].getValor()), 200);
+			valorFaixa = new JTextField(String.valueOf(dados.getFilmes()[pos].getFaixaEtaria()), 200);
+			valorDim = new JTextField(dados.getFilmes()[pos].getDimensao(), 200);
+			valorAudio = new JTextField(dados.getFilmes()[pos].getAudio(), 200);
+			valorEspCad = new JTextField(String.valueOf(dados.getFilmes()[pos].getEspCad()), 200);
+
+		} /*
+			 * else if (op == 4) { // Preenche dados com dados do professor clicado
+			 * valorNome = new JTextField(dados.getProfessores()[pos].getNome(), 200);
+			 * valorHoraAula = new
+			 * JTextField(String.valueOf(dados.getProfessores()[pos].getValorHoraAula()),
+			 * 200); valorEnd = new JTextField(200); valorCPF = new
+			 * JTextField(String.valueOf(dados.getProfessores()[pos].getCPF()), 200);
+			 * valorID = new
+			 * JTextField(String.valueOf(dados.getProfessores()[pos].getNumID()), 200);
+			 * valorDDD = new
+			 * JTextField(String.valueOf(dados.getProfessores()[pos].getNumTel().getDDD()),
+			 * 3); valorTelefone = new
+			 * JTextField(String.valueOf(dados.getProfessores()[pos].getNumTel().getNumero()
+			 * ), 10);
+			 * 
+			 * }
+			 */ else { // Não preenche com dados
+
+			valorNome = new JTextField(200);
+			valorSala = new JTextField(200);
+			valorHora = new JTextField(200);
+			valorDura = new JTextField(200);
+			valorGenero = new JTextField(200);
+			valorValor = new JTextField(200);
+			valorFaixa = new JTextField(200);
+			valorDim = new JTextField(200);
+			valorAudio = new JTextField(200);
+			valorEspCad = new JTextField(200);
+
+		}
+
+		labelNome.setBounds(30, 20, 100, 25);
+		valorNome.setBounds(136, 20, 200, 25);
+		labelSala.setBounds(30, 50, 100, 25);
+		valorSala.setBounds(136, 50, 200, 25);
+		labelHora.setBounds(30, 80, 100, 25);
+		valorHora.setBounds(136, 80, 200, 25);
+		labelDura.setBounds(30, 110, 100, 25);
+		valorDura.setBounds(136, 110, 200, 25);
+		labelGenero.setBounds(30, 140, 100, 25);
+		valorGenero.setBounds(136, 140, 200, 25);
+		Labelvalor.setBounds(30, 170, 100, 25);
+		valorValor.setBounds(136, 170, 200, 25);
+		labelFaixa.setBounds(30, 200, 100, 25);
+		valorFaixa.setBounds(136, 200, 200, 25);
+		labelDim.setBounds(30, 230, 100, 25);
+		valorDim.setBounds(136, 230, 200, 25);
+		labelAudio.setBounds(30, 260, 100, 25);
+		valorAudio.setBounds(136, 260, 200, 25);
+		labelEspCad.setBounds(30, 290, 100, 25);
+		valorEspCad.setBounds(136, 290, 200, 25);
+		botaoSalvar.setBounds(220, 340, 115, 30);
+
+		/*
+		 * // Coloca os campos relacionados a endereco se aluno if (op == 1 || op == 3)
+		 * { this.janela.add(labelEnd); this.janela.add(valorEnd);
+		 * 
+		 * }
+		 * 
+		 * // Coloca botoes de excluir e salvar if (op == 3 || op == 4) {
+		 * botaoSalvar.setBounds(120, 175, 115, 30); botaoExcluir.setBounds(245, 175,
+		 * 115, 30); this.janela.add(botaoExcluir); }
+		 */
 		this.janela.add(labelNome);
 		this.janela.add(valorNome);
+		this.janela.add(labelSala);
+		this.janela.add(valorSala);
+		this.janela.add(labelHora);
+		this.janela.add(valorHora);
+		this.janela.add(labelDura);
+		this.janela.add(valorDura);
+		this.janela.add(labelGenero);
+		this.janela.add(valorGenero);
 
-		this.janela.add(labelCPF);
-		this.janela.add(valorCPF);
-		this.janela.add(labelID);
-		this.janela.add(valorID);
-		this.janela.add(labelTelefone);
-		this.janela.add(valorDDD);
-		this.janela.add(valorTelefone);
+		this.janela.add(Labelvalor);
+		this.janela.add(valorValor);
+		this.janela.add(labelFaixa);
+		this.janela.add(valorFaixa);
+		this.janela.add(labelDim);
+		this.janela.add(valorDim);
+		this.janela.add(labelAudio);
+		this.janela.add(valorAudio);
+		this.janela.add(labelEspCad);
+		this.janela.add(valorEspCad);
+
 		this.janela.add(botaoSalvar);
 
 		this.janela.setLayout(null);
 
-		this.janela.setSize(400, 250);
+		this.janela.setSize(400, 430);
 		this.janela.setVisible(true);
+		janela.setResizable(false);
+		janela.setLocationRelativeTo(null);
 
 		botaoSalvar.addActionListener(this);
 		botaoExcluir.addActionListener(this);
-
 	}
-	/*
-	 * public void inserirEditar(int op, ControleDados d, TelaIngresso telaIngresso,
-	 * int pos) {
-	 * 
-	 * opcao = op; posicao = pos; dados = d;
-	 * 
-	 * if (op == 1) dado = "Cadastro de Filme"; if (op == 2) dado =
-	 * "Cadastro de Ingresso"; if (op == 3) dado = "Cadastro Acompanhamento"; if (op
-	 * == 4) dado = "Edição Filme"; if (op == 5) dado = "Edição Ingresso"; if (op ==
-	 * 6) dado = "Edição Acompanhamento";
-	 * 
-	 * }
-	 */
-	/*
-	 * //Preenche dados com dados do aluno clicado if (op == 3) { valorNome = new
-	 * JTextField(dados.getAlunos()[pos].getNome(), 200); valorEnd = new
-	 * JTextField(dados.getAlunos()[pos].getEndereco(),200); valorHoraAula = new
-	 * JTextField(200); valorCPF = new
-	 * JTextField(String.valueOf(dados.getAlunos()[pos].getCPF()), 200); valorID =
-	 * new JTextField(String.valueOf(dados.getAlunos()[pos].getNumID()), 200);
-	 * valorDDD = new
-	 * JTextField(String.valueOf(dados.getAlunos()[pos].getNumTel().getDDD()), 3);
-	 * valorTelefone = new
-	 * JTextField(String.valueOf(dados.getAlunos()[pos].getNumTel().getNumero()),
-	 * 10);
-	 * 
-	 * } else if (op == 4) { //Preenche dados com dados do professor clicado
-	 * valorNome = new JTextField(dados.getProfessores()[pos].getNome(), 200);
-	 * valorHoraAula = new JTextField(String.valueOf(
-	 * dados.getProfessores()[pos].getValorHoraAula()),200); valorEnd = new
-	 * JTextField(200); valorCPF = new
-	 * JTextField(String.valueOf(dados.getProfessores()[pos].getCPF()), 200);
-	 * valorID = new
-	 * JTextField(String.valueOf(dados.getProfessores()[pos].getNumID()), 200);
-	 * valorDDD = new JTextField(
-	 * String.valueOf(dados.getProfessores()[pos].getNumTel().getDDD()), 3);
-	 * valorTelefone = new JTextField(
-	 * String.valueOf(dados.getProfessores()[pos].getNumTel().getNumero()), 10);
-	 * 
-	 * } else { //Não preenche com dados
-	 * 
-	 * valorNome = new JTextField(200); valorEnd = new JTextField(200);
-	 * valorHoraAula = new JTextField(200); valorCPF = new JTextField(200); valorID
-	 * = new JTextField(200); valorDDD = new JTextField(3); valorTelefone = new
-	 * JTextField(10);
-	 * 
-	 * botaoSalvar.setBounds(245, 175, 115, 30);
-	 * 
-	 * 
-	 * labelNome.setBounds(30, 20, 150, 25); valorNome.setBounds(180, 20, 180, 25);
-	 * labelEnd.setBounds(30, 50, 150, 25); valorEnd.setBounds(180, 50, 180, 25);
-	 * labelHoraAula.setBounds(30, 50, 180, 25); valorHoraAula.setBounds(180, 50,
-	 * 180, 25); labelCPF.setBounds(30, 80, 150, 25); valorCPF.setBounds(180, 80,
-	 * 180, 25); labelID.setBounds(30, 110, 150, 25); valorID.setBounds(180, 110,
-	 * 180, 25); labelTelefone.setBounds(30, 140, 150, 25); valorDDD.setBounds(180,
-	 * 140, 28, 25); valorTelefone.setBounds(210, 140, 65, 25);
-	 * 
-	 * //Coloca os campos relacionados a endereco se aluno if (op == 1 || op == 3 )
-	 * { this.janela.add(labelEnd); this.janela.add(valorEnd);
-	 * 
-	 * }
-	 * 
-	 * //Coloca campos relacionados a valor hora/aula se professor if (op == 2 || op
-	 * == 4) {
-	 * 
-	 * this.janela.add(labelHoraAula); this.janela.add(valorHoraAula); }
-	 * 
-	 * //Coloca botoes de excluir e salvar if (op == 3 || op == 4) {
-	 * botaoSalvar.setBounds(120, 175, 115, 30); botaoExcluir.setBounds(245, 175,
-	 * 115, 30); this.janela.add(botaoExcluir); }
-	 */
-
-	/*
-	 * @Override public void actionPerformed(ActionEvent e) { Object src =
-	 * e.getSource(); if (src == botaoSalvar) { try { boolean res; if (opcao == 1)
-	 * // cadastro de novo aluno novoDado[0] =
-	 * Integer.toString(dados.getQtdAlunos()); else if (opcao == 2) // cadastro de
-	 * novo prof novoDado[0] = Integer.toString(dados.getQtdProfs()); else // edicao
-	 * de dado existente novoDado[0] = Integer.toString(posicao);
-	 * 
-	 * novoDado[1] = valorNome.getText(); novoDado[3] = valorCPF.getText();
-	 * novoDado[4] = valorID.getText(); novoDado[5] = valorDDD.getText();
-	 * novoDado[6] = valorTelefone.getText();
-	 * 
-	 * if (opcao == 1 || opcao == 3) { novoDado[2] = valorEnd.getText(); res =
-	 * dados.inserirEditarAluno(novoDado); } else { novoDado[2] =
-	 * valorHoraAula.getText(); res = dados.inserirEditarProf(novoDado); }
-	 * 
-	 * if (res) { mensagemSucessoCadastro(); } else mensagemErroCadastro();
-	 * 
-	 * } catch (NullPointerException exc1) { mensagemErroCadastro(); } catch
-	 * (NumberFormatException exc2) { mensagemErroCadastro(); } }
-	 * 
-	 * if (src == botaoExcluir) { boolean res = false;
-	 * 
-	 * if (opcao == 3) {// exclui aluno res = dados.removerAluno(posicao); if (res)
-	 * mensagemSucessoExclusao(); else mensagemErroExclusaoAluno(); }
-	 * 
-	 * if (opcao == 4) { // exclui professor res = dados.removerProfessor(posicao);
-	 * if (res) mensagemSucessoExclusao(); else mensagemErroExclusaoProf(); }
-	 * 
-	 * } }
-	 */
-	/*
-	 * public void mensagemSucessoExclusao() { JOptionPane.showMessageDialog(null,
-	 * "Os dados foram excluidos com sucesso!", null,
-	 * JOptionPane.INFORMATION_MESSAGE); janela.dispose(); }
-	 * 
-	 * public void mensagemSucessoCadastro() { JOptionPane.showMessageDialog(null,
-	 * "Os dados foram salvos com sucesso!", null, JOptionPane.INFORMATION_MESSAGE);
-	 * janela.dispose(); }
-	 * 
-	 * public void mensagemErroCadastro() { JOptionPane.showMessageDialog(null,
-	 * "ERRO AO SALVAR OS DADOS!\n " +
-	 * "Pode ter ocorrido um dos dois erros a seguir:  \n" +
-	 * "1. Nem todos os campos foram preenchidos \n" +
-	 * "2. CPF, identidade, DDD e telefone não contém apenas números", null,
-	 * JOptionPane.ERROR_MESSAGE); }
-	 * 
-	 * public void mensagemErroExclusaoAluno() { JOptionPane.showMessageDialog(null,
-	 * "Ocorreu um erro ao excluir o dado.\n " +
-	 * "Verifique se o aluno está matriculado\n" +
-	 * "em alguma disciplina. Se sim, cancele\n " +
-	 * "a matricula e tente novamente.", null, JOptionPane.ERROR_MESSAGE); }
-	 * 
-	 * public void mensagemErroExclusaoProf() { JOptionPane.showMessageDialog(null,
-	 * "Ocorreu um erro ao excluir o dado.\n " +
-	 * "Verifique se o professor está responsável\n" +
-	 * "por alguma disciplina. Se sim, substitua\n " +
-	 * "o professor e tente novamente.", null, JOptionPane.ERROR_MESSAGE); }
-	 */
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		if (src == botaoSalvar) {
+			try {
+				boolean res;
+				if (opcao == 1) // cadastro de novo aluno
+					novoDado[0] = Integer.toString(dados.getQtdAlunos());
+				else if (opcao == 2) // cadastro de novo prof
+					novoDado[0] = Integer.toString(dados.getQtdProfs());
+				else // edicao de dado existente
+					novoDado[0] = Integer.toString(posicao);
 
+				novoDado[1] = valorNome.getText();
+				novoDado[3] = valorCPF.getText();
+				novoDado[4] = valorID.getText();
+				novoDado[5] = valorDDD.getText();
+				novoDado[6] = valorTelefone.getText();
+
+				if (opcao == 1 || opcao == 3) {
+					novoDado[2] = valorEnd.getText();
+					res = dados.inserirEditarAluno(novoDado);
+				} else {
+					novoDado[2] = valorHoraAula.getText();
+					res = dados.inserirEditarProf(novoDado);
+				}
+
+				if (res) {
+					mensagemSucessoCadastro();
+				} else
+					mensagemErroCadastro();
+
+			} catch (NullPointerException exc1) {
+				mensagemErroCadastro();
+			} catch (NumberFormatException exc2) {
+				mensagemErroCadastro();
+			}
+		}
+
+		if (src == botaoExcluir) {
+			boolean res = false;
+
+			if (opcao == 3) {// exclui aluno
+				res = dados.removerAluno(posicao);
+				if (res)
+					mensagemSucessoExclusao();
+				else
+					mensagemErroExclusaoAluno();
+			}
+
+			if (opcao == 4) { // exclui professor
+				res = dados.removerProfessor(posicao);
+				if (res)
+					mensagemSucessoExclusao();
+				else
+					mensagemErroExclusaoProf();
+			}
+
+		}
 	}
+
+	public void mensagemSucessoExclusao() {
+		JOptionPane.showMessageDialog(null, "Os dados foram excluidos com sucesso!", null,
+				JOptionPane.INFORMATION_MESSAGE);
+		janela.dispose();
+	}
+
+	public void mensagemSucessoCadastro() {
+		JOptionPane.showMessageDialog(null, "Os dados foram salvos com sucesso!", null,
+				JOptionPane.INFORMATION_MESSAGE);
+		janela.dispose();
+	}
+
+	public void mensagemErroCadastro() {
+		JOptionPane.showMessageDialog(null,
+				"ERRO AO SALVAR OS DADOS!\n " + "Pode ter ocorrido um dos dois erros a seguir:  \n"
+						+ "1. Nem todos os campos foram preenchidos \n"
+						+ "2. CPF, identidade, DDD e telefone não contém apenas números",
+				null, JOptionPane.ERROR_MESSAGE);
+	}
+
+	public void mensagemErroExclusaoAluno() {
+		JOptionPane.showMessageDialog(null,
+				"Ocorreu um erro ao excluir o dado.\n " + "Verifique se o aluno está matriculado\n"
+						+ "em alguma disciplina. Se sim, cancele\n " + "a matricula e tente novamente.",
+				null, JOptionPane.ERROR_MESSAGE);
+	}
+
+	public void mensagemErroExclusaoProf() {
+		JOptionPane.showMessageDialog(null,
+				"Ocorreu um erro ao excluir o dado.\n " + "Verifique se o professor está responsável\n"
+						+ "por alguma disciplina. Se sim, substitua\n " + "o professor e tente novamente.",
+				null, JOptionPane.ERROR_MESSAGE);
+	}
+
 }
--- src/modelo/Filme.java
+++ src/modelo/Filme.java
@@ -31,10 +31,10 @@ public class Filme extends Cadastro {
 
 	public Filme(int numSala, String nomeFilme, String horaFilme, int duracao, String genero, double valor,
 			int faixaEtaria, String d, String audio, int espCad) {
-		this.setNumSala(numSala);
-		this.setHoraFilme(horaFilme);
-		this.setNomeFilme(nomeFilme);
-		this.setDuracao(duracao);
+		this.setNumSala(numSala);//
+		this.setHoraFilme(horaFilme);//
+		this.setNomeFilme(nomeFilme);//
+		this.setDuracao(duracao);//
 		this.setGenero(genero);
 		this.setValor(valor);
 		this.setFaixaEtaria(faixaEtaria);
